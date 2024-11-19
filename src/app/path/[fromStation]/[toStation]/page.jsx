0f2eb39115e07516748c9a6dbedf505afbe3ff63
Dhruv Gupta <dhruv.gupta2711@gmail.com>
2024-11-20 01:47:04 +0530

    static pages

--- src/app/path/[fromStation]/[toStation]/page.jsx
+++ src/app/path/[fromStation]/[toStation]/page.jsx
@@ -3,17 +3,17 @@ import CheckStations from './searchComp';
 import selectOptions from '@/app/data/selectOptions';
 import { CodeSquare } from 'lucide-react';
 
-export async function generateStaticParams() {
-  const pairs = selectOptions.flatMap((stationStart) =>
-    selectOptions.map((stationEnd) => ({
-      fromStation: stationStart.label,
-      toStation: stationEnd.label
-    }))
-  ).filter(pair => pair.fromStation !== pair.toStation);
+// export async function generateStaticParams() {
+//   const pairs = selectOptions.flatMap((stationStart) =>
+//     selectOptions.map((stationEnd) => ({
+//       fromStation: stationStart.label,
+//       toStation: stationEnd.label
+//     }))
+//   ).filter(pair => pair.fromStation !== pair.toStation);
 
-  // console.log(pairs);
-  return pairs;
-}
+//   // console.log(pairs);
+//   return pairs;
+// }
 
 export async function generateMetadata({ params }) {
   const fromStation = params.fromStation.replace(/%20/g, " ")
